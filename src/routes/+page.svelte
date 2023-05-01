2bd8068f40ae9be77f8445065b5f32c7397507e8
timabloom <timabloom@proton.me>
2023-05-01 18:07:28 +0200

    updated introduction text and removed unnecessary html

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,14 @@
 <script></script>
 
 <svelte:head>
-	<title>Tarot Sanctuary - For Lovers of the Tarot</title>
+	<title>Tarot Sanctuary - Open and free for all</title>
 </svelte:head>
 
 <header>
 	<h1>Welcome, dear one to the Tarot Sanctuary!</h1>
 	<p>
 		Before we embark on our Tarot journey together, I want to invite you to consider these three
-		guiding maxims that can help us use this powerful tool with care and intention. By keeping these
+		guiding maxims: Know thyself, Nothing in excess and Surety brings ruin. By keeping these
 		principles in mind, we can approach the Tarot with love and respect, and open ourselves up to
 		its deep wisdom and transformative potential. So let's take a deep breath, center ourselves, and
 		embark on this beautiful journey of discovery and self-exploration together!
@@ -18,20 +18,8 @@
 <main>
 	<h2>The Three Well Known Delphic Maxims</h2>
 	<h3>Know Thyself</h3>
-	<p>
-		The first maxim reminds us to look within and know ourselves deeply, so that we can better
-		understand the messages and symbols of the Tarot.
-	</p>
 	<h3>Nothing in Excess</h3>
-	<p>
-		The second maxim advises us to use the Tarot in moderation, and not rely on it as our only
-		source of guidance or decision-making.
-	</p>
 	<h3>Surety Brings Ruin</h3>
-	<p>
-		The third maxim cautions us against being too certain or rigid in our interpretations of the
-		Tarot, and encourages us to stay open and receptive to new insights and perspectives.
-	</p>
 </main>
 
 <style></style>
