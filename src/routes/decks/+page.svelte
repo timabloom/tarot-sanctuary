c88c129a0280612d778915a1084817ea3bd4ed3f
timabloom <timabloom@proton.me>
2023-05-05 03:54:29 +0200

    made all cards visible on screen

--- src/routes/decks/+page.svelte
+++ src/routes/decks/+page.svelte
@@ -1,9 +1,45 @@
-<script></script>
+<script lang="ts">
+	import { marseilleDeck } from "../../lib/marseille-deck";
+</script>
 
 <svelte:head>
 	<title>Decks - Description</title>
 </svelte:head>
 
-<h2>Decks</h2>
+<header>
+	<h1>Tarot de Marseille Deck</h1>
+</header>
 
-<style></style>
+<main>
+	<div class="cards-container">
+		{#each marseilleDeck as card (card)}
+			<div class="card">
+				<img alt={card.name} src={card.image} />
+				<h2>{card.name}</h2>
+			</div>
+		{/each}
+	</div>
+</main>
+
+<style>
+	header {
+		text-align: center;
+	}
+	.cards-container {
+		display: flex;
+		flex-wrap: wrap;
+		justify-content: center;
+		padding: 1em 3em 2em 3em;
+		gap: 1em;
+	}
+	.card {
+		max-width: 200px;
+		text-align: center;
+	}
+	.card img {
+		max-width: 100%;
+	}
+	.card h2 {
+		margin-top: 0.5em;
+	}
+</style>
