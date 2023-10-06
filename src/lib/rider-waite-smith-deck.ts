1ad580e2633ba468d24168a19d88ef45d100b0c9
timabloom <timabloom@proton.me>
2023-10-06 20:23:33 +0200

    webp

--- src/lib/rider-waite-smith-deck.ts
+++ src/lib/rider-waite-smith-deck.ts
@@ -3,396 +3,396 @@ import type { Cards } from "../types/cards";
 export const riderWaiteSmithDeck: Cards[] = [
 	{
 		name: "The Fool",
-		image: "./rider-waite-smith-deck/0-the-fool.jpg",
+		image: "./rider-waite-smith-deck/0-the-fool.webp",
 		arcana: "major"
 	},
 	{
 		name: "The Magician",
-		image: "./rider-waite-smith-deck/1-the-magician.jpg",
+		image: "./rider-waite-smith-deck/1-the-magician.webp",
 		arcana: "major"
 	},
 	{
 		name: "The High Priestess",
-		image: "./rider-waite-smith-deck/2-the-high-priestess.jpg",
+		image: "./rider-waite-smith-deck/2-the-high-priestess.webp",
 		arcana: "major"
 	},
 	{
 		name: "The Empress",
-		image: "./rider-waite-smith-deck/3-the-empress.jpg",
+		image: "./rider-waite-smith-deck/3-the-empress.webp",
 		arcana: "major"
 	},
 	{
 		name: "The Emperor",
-		image: "./rider-waite-smith-deck/4-the-emperor.jpg",
+		image: "./rider-waite-smith-deck/4-the-emperor.webp",
 		arcana: "major"
 	},
 	{
 		name: "The Hierophant",
-		image: "./rider-waite-smith-deck/5-the-hierophant.jpg",
+		image: "./rider-waite-smith-deck/5-the-hierophant.webp",
 		arcana: "major"
 	},
 	{
 		name: "The Lovers",
-		image: "./rider-waite-smith-deck/6-the-lovers.jpg",
+		image: "./rider-waite-smith-deck/6-the-lovers.webp",
 		arcana: "major"
 	},
 	{
 		name: "The Chariot",
-		image: "./rider-waite-smith-deck/7-the-chariot.jpg",
+		image: "./rider-waite-smith-deck/7-the-chariot.webp",
 		arcana: "major"
 	},
 	{
 		name: "Strength",
-		image: "./rider-waite-smith-deck/8-strength.jpg",
+		image: "./rider-waite-smith-deck/8-strength.webp",
 		arcana: "major"
 	},
 	{
 		name: "The Hermit",
-		image: "./rider-waite-smith-deck/9-the-hermit.jpg",
+		image: "./rider-waite-smith-deck/9-the-hermit.webp",
 		arcana: "major"
 	},
 	{
 		name: "Wheel of Fortune",
-		image: "./rider-waite-smith-deck/10-wheel-of-fortune.jpg",
+		image: "./rider-waite-smith-deck/10-the-wheel-of-fortune.webp",
 		arcana: "major"
 	},
 	{
 		name: "Justice",
-		image: "./rider-waite-smith-deck/11-justice.jpg",
+		image: "./rider-waite-smith-deck/11-justice.webp",
 		arcana: "major"
 	},
 	{
 		name: "The Hanged Man",
-		image: "./rider-waite-smith-deck/12-the-hanged-man.jpg",
+		image: "./rider-waite-smith-deck/12-the-hanged-man.webp",
 		arcana: "major"
 	},
 	{
 		name: "Death",
-		image: "./rider-waite-smith-deck/13-death.jpg",
+		image: "./rider-waite-smith-deck/13-death.webp",
 		arcana: "major"
 	},
 	{
 		name: "Temperance",
-		image: "./rider-waite-smith-deck/14-temperance.jpg",
+		image: "./rider-waite-smith-deck/14-temperance.webp",
 		arcana: "major"
 	},
 	{
 		name: "The Devil",
-		image: "./rider-waite-smith-deck/15-the-devil.jpg",
+		image: "./rider-waite-smith-deck/15-the-devil.webp",
 		arcana: "major"
 	},
 	{
 		name: "The Tower",
-		image: "./rider-waite-smith-deck/16-the-tower.jpg",
+		image: "./rider-waite-smith-deck/16-the-tower.webp",
 		arcana: "major"
 	},
 	{
 		name: "The Star",
-		image: "./rider-waite-smith-deck/17-the-star.jpg",
+		image: "./rider-waite-smith-deck/17-the-star.webp",
 		arcana: "major"
 	},
 	{
 		name: "The Moon",
-		image: "./rider-waite-smith-deck/18-the-moon.jpg",
+		image: "./rider-waite-smith-deck/18-the-moon.webp",
 		arcana: "major"
 	},
 	{
 		name: "The Sun",
-		image: "./rider-waite-smith-deck/19-the-sun.jpg",
+		image: "./rider-waite-smith-deck/19-the-sun.webp",
 		arcana: "major"
 	},
 	{
 		name: "Judgement",
-		image: "./rider-waite-smith-deck/20-judgement.jpg",
+		image: "./rider-waite-smith-deck/20-judgement.webp",
 		arcana: "major"
 	},
 	{
 		name: "The World",
-		image: "./rider-waite-smith-deck/21-the-world.jpg",
+		image: "./rider-waite-smith-deck/21-the-world.webp",
 		arcana: "major"
 	},
 	{
 		name: "Ace of Wands",
-		image: "./rider-waite-smith-deck/22-ace-of-wands.jpg",
+		image: "./rider-waite-smith-deck/22-ace-of-wands.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Two of Wands",
-		image: "./rider-waite-smith-deck/23-two-of-wands.jpg",
+		image: "./rider-waite-smith-deck/23-two-of-wands.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Three of Wands",
-		image: "./rider-waite-smith-deck/24-three-of-wands.jpg",
+		image: "./rider-waite-smith-deck/24-three-of-wands.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Four of Wands",
-		image: "./rider-waite-smith-deck/25-four-of-wands.jpg",
+		image: "./rider-waite-smith-deck/25-four-of-wands.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Five of Wands",
-		image: "./rider-waite-smith-deck/26-five-of-wands.jpg",
+		image: "./rider-waite-smith-deck/26-five-of-wands.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Six of Wands",
-		image: "./rider-waite-smith-deck/27-six-of-wands.jpg",
+		image: "./rider-waite-smith-deck/27-six-of-wands.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Seven of Wands",
-		image: "./rider-waite-smith-deck/28-seven-of-wands.jpg",
+		image: "./rider-waite-smith-deck/28-seven-of-wands.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Eight of Wands",
-		image: "./rider-waite-smith-deck/29-eight-of-wands.jpg",
+		image: "./rider-waite-smith-deck/29-eight-of-wands.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Nine of Wands",
-		image: "./rider-waite-smith-deck/30-nine-of-wands.jpg",
+		image: "./rider-waite-smith-deck/30-nine-of-wands.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Ten of Wands",
-		image: "./rider-waite-smith-deck/31-ten-of-wands.jpg",
+		image: "./rider-waite-smith-deck/31-ten-of-wands.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Page of Wands",
-		image: "./rider-waite-smith-deck/32-page-of-wands.jpg",
+		image: "./rider-waite-smith-deck/32-page-of-wands.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Knight of Wands",
-		image: "./rider-waite-smith-deck/33-knight-of-wands.jpg",
+		image: "./rider-waite-smith-deck/33-knight-of-wands.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Queen of Wands",
-		image: "./rider-waite-smith-deck/34-queen-of-wands.jpg",
+		image: "./rider-waite-smith-deck/34-queen-of-wands.webp",
 		arcana: "minor"
 	},
 	{
 		name: "King of Wands",
-		image: "./rider-waite-smith-deck/35-king-of-wands.jpg",
+		image: "./rider-waite-smith-deck/35-king-of-wands.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Ace of Pentacles",
-		image: "./rider-waite-smith-deck/36-ace-of-pentacles.jpg",
+		image: "./rider-waite-smith-deck/36-ace-of-pentacles.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Two of Pentacles",
-		image: "./rider-waite-smith-deck/37-two-of-pentacles.jpg",
+		image: "./rider-waite-smith-deck/37-two-of-pentacles.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Three of Pentacles",
-		image: "./rider-waite-smith-deck/38-three-of-pentacles.jpg",
+		image: "./rider-waite-smith-deck/38-three-of-pentacles.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Four of Pentacles",
-		image: "./rider-waite-smith-deck/39-four-of-pentacles.jpg",
+		image: "./rider-waite-smith-deck/39-four-of-pentacles.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Five of Pentacles",
-		image: "./rider-waite-smith-deck/40-five-of-pentacles.jpg",
+		image: "./rider-waite-smith-deck/40-five-of-pentacles.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Six of Pentacles",
-		image: "./rider-waite-smith-deck/41-six-of-pentacles.jpg",
+		image: "./rider-waite-smith-deck/41-six-of-pentacles.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Seven of Pentacles",
-		image: "./rider-waite-smith-deck/42-seven-of-pentacles.jpg",
+		image: "./rider-waite-smith-deck/42-seven-of-pentacles.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Eight of Pentacles",
-		image: "./rider-waite-smith-deck/43-eight-of-pentacles.jpg",
+		image: "./rider-waite-smith-deck/43-eight-of-pentacles.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Nine of Pentacles",
-		image: "./rider-waite-smith-deck/44-nine-of-pentacles.jpg",
+		image: "./rider-waite-smith-deck/44-nine-of-pentacles.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Ten of Pentacles",
-		image: "./rider-waite-smith-deck/45-ten-of-pentacles.jpg",
+		image: "./rider-waite-smith-deck/45-ten-of-pentacles.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Page of Pentacles",
-		image: "./rider-waite-smith-deck/46-page-of-pentacles.jpg",
+		image: "./rider-waite-smith-deck/46-page-of-pentacles.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Knight of Pentacles",
-		image: "./rider-waite-smith-deck/47-knight-of-pentacles.jpg",
+		image: "./rider-waite-smith-deck/47-knight-of-pentacles.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Queen of Pentacles",
-		image: "./rider-waite-smith-deck/48-queen-of-pentacles.jpg",
+		image: "./rider-waite-smith-deck/48-queen-of-pentacles.webp",
 		arcana: "minor"
 	},
 	{
 		name: "King of Pentacles",
-		image: "./rider-waite-smith-deck/49-king-of-pentacles.jpg",
+		image: "./rider-waite-smith-deck/49-king-of-pentacles.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Ace of Cups",
-		image: "./rider-waite-smith-deck/50-ace-of-cups.jpg",
+		image: "./rider-waite-smith-deck/50-ace-of-cups.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Two of Cups",
-		image: "./rider-waite-smith-deck/51-two-of-cups.jpg",
+		image: "./rider-waite-smith-deck/51-two-of-cups.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Three of Cups",
-		image: "./rider-waite-smith-deck/52-three-of-cups.jpg",
+		image: "./rider-waite-smith-deck/52-three-of-cups.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Four of Cups",
-		image: "./rider-waite-smith-deck/53-four-of-cups.jpg",
+		image: "./rider-waite-smith-deck/53-four-of-cups.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Five of Cups",
-		image: "./rider-waite-smith-deck/54-five-of-cups.jpg",
+		image: "./rider-waite-smith-deck/54-five-of-cups.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Six of Cups",
-		image: "./rider-waite-smith-deck/55-six-of-cups.jpg",
+		image: "./rider-waite-smith-deck/55-six-of-cups.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Seven of Cups",
-		image: "./rider-waite-smith-deck/56-seven-of-cups.jpg",
+		image: "./rider-waite-smith-deck/56-seven-of-cups.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Eight of Cups",
-		image: "./rider-waite-smith-deck/57-eight-of-cups.jpg",
+		image: "./rider-waite-smith-deck/57-eight-of-cups.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Nine of Cups",
-		image: "./rider-waite-smith-deck/58-nine-of-cups.jpg",
+		image: "./rider-waite-smith-deck/58-nine-of-cups.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Ten of Cups",
-		image: "./rider-waite-smith-deck/59-ten-of-cups.jpg",
+		image: "./rider-waite-smith-deck/59-ten-of-cups.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Page of Cups",
-		image: "./rider-waite-smith-deck/60-page-of-cups.jpg",
+		image: "./rider-waite-smith-deck/60-page-of-cups.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Knight of Cups",
-		image: "./rider-waite-smith-deck/61-knight-of-cups.jpg",
+		image: "./rider-waite-smith-deck/61-knight-of-cups.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Queen of Cups",
-		image: "./rider-waite-smith-deck/62-queen-of-cups.jpg",
+		image: "./rider-waite-smith-deck/62-queen-of-cups.webp",
 		arcana: "minor"
 	},
 	{
 		name: "King of Cups",
-		image: "./rider-waite-smith-deck/63-king-of-cups.jpg",
+		image: "./rider-waite-smith-deck/63-king-of-cups.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Ace of Swords",
-		image: "./rider-waite-smith-deck/64-ace-of-swords.jpg",
+		image: "./rider-waite-smith-deck/64-ace-of-swords.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Two of Swords",
-		image: "./rider-waite-smith-deck/65-two-of-swords.jpg",
+		image: "./rider-waite-smith-deck/65-two-of-swords.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Three of Swords",
-		image: "./rider-waite-smith-deck/66-three-of-swords.jpg",
+		image: "./rider-waite-smith-deck/66-three-of-swords.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Four of Swords",
-		image: "./rider-waite-smith-deck/67-four-of-swords.jpg",
+		image: "./rider-waite-smith-deck/67-four-of-swords.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Five of Swords",
-		image: "./rider-waite-smith-deck/68-five-of-swords.jpg",
+		image: "./rider-waite-smith-deck/68-five-of-swords.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Six of Swords",
-		image: "./rider-waite-smith-deck/69-six-of-swords.jpg",
+		image: "./rider-waite-smith-deck/69-six-of-swords.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Seven of Swords",
-		image: "./rider-waite-smith-deck/70-seven-of-swords.jpg",
+		image: "./rider-waite-smith-deck/70-seven-of-swords.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Eight of Swords",
-		image: "./rider-waite-smith-deck/71-eight-of-swords.jpg",
+		image: "./rider-waite-smith-deck/71-eight-of-swords.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Nine of Swords",
-		image: "./rider-waite-smith-deck/72-nine-of-swords.jpg",
+		image: "./rider-waite-smith-deck/72-nine-of-swords.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Ten of Swords",
-		image: "./rider-waite-smith-deck/73-ten-of-swords.jpg",
+		image: "./rider-waite-smith-deck/73-ten-of-swords.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Page of Swords",
-		image: "./rider-waite-smith-deck/74-page-of-swords.jpg",
+		image: "./rider-waite-smith-deck/74-page-of-swords.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Knight of Swords",
-		image: "./rider-waite-smith-deck/75-knight-of-swords.jpg",
+		image: "./rider-waite-smith-deck/75-knight-of-swords.webp",
 		arcana: "minor"
 	},
 	{
 		name: "Queen of Swords",
-		image: "./rider-waite-smith-deck/76-queen-of-swords.jpg",
+		image: "./rider-waite-smith-deck/76-queen-of-swords.webp",
 		arcana: "minor"
 	},
 	{
 		name: "King of Swords",
-		image: "./rider-waite-smith-deck/77-king-of-swords.jpg",
+		image: "./rider-waite-smith-deck/77-king-of-swords.webp",
 		arcana: "minor"
 	}
 ];
 
 export const riderWaiteSmithCardBack: Cards[] = [
-	{ id: 1, name: "Card Back", image: "./rider-waite-smith-deck/00-the-back.jpg" }
+	{ id: 1, name: "Card Back", image: "./rider-waite-smith-deck/00-the-back.webp" }
 ];
