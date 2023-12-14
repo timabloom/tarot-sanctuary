b2c5b6919ca47a227a23920507f99e845d73e947
timabloom <timabloom@proton.me>
2023-12-14 01:18:50 +0100

    new decks navigation

--- src/routes/decks/+page.svelte
+++ src/routes/decks/+page.svelte
@@ -2,10 +2,14 @@
 	import { marseilleDeck } from "../../lib/marseille-deck";
 	import { riderWaiteSmithDeck } from "$lib/rider-waite-smith-deck";
 
+	import type { Cards } from "../../types/cards";
+
 	let deck = marseilleDeck;
 	let name = "Tarot de Marseille Deck";
 	let attributionLink = "https://cbdtarot.com";
 	let attributionName = "CBD Tarot de Marseille by Dr. Yoav Ben-Dov";
+	let cardDeckCSS = "marseille";
+	let selectedCard = deck[0];
 
 	function switchDeck(deckSwitch: string): void {
 		if (deckSwitch === "marseille") {
@@ -13,11 +17,35 @@
 			name = "Tarot de Marseille Deck";
 			attributionLink = "https://cbdtarot.com";
 			attributionName = "CBD Tarot de Marseille by Dr. Yoav Ben-Dov";
+			cardDeckCSS = "marseille";
+			selectedCard = deck[0];
 		} else if (deckSwitch === "rider-waite-smith") {
 			deck = riderWaiteSmithDeck;
 			name = "Rider Waite Smith Deck";
 			attributionLink = "/attributions";
 			attributionName = "Enhanced Rider-Waite Tarot by Timothy Magee";
+			cardDeckCSS = "rider-waite-smith";
+			selectedCard = deck[0];
+		}
+	}
+
+	function handleCardClick(card: Cards) {
+		selectedCard = card;
+		const element = document.getElementById("scroll-to");
+		if (element) {
+			element.scrollIntoView({ behavior: "smooth" });
+		} else console.log("element not found");
+	}
+
+	function handleArrowClick(changeCard: string) {
+		if (changeCard === "previous") {
+			if (deck.indexOf(selectedCard) === 0) {
+				selectedCard = deck[77];
+			} else selectedCard = deck[deck.indexOf(selectedCard) - 1];
+		} else if (changeCard === "next") {
+			if (deck.indexOf(selectedCard) === 77) {
+				selectedCard = deck[0];
+			} else selectedCard = deck[deck.indexOf(selectedCard) + 1];
 		}
 	}
 </script>
@@ -29,7 +57,7 @@
 <header>
 	<div class="options-container">
 		<h1>Tarot Decks</h1>
-		<div>
+		<div id="scroll-to">
 			<label>
 				<input type="radio" name="deck" checked on:change={() => switchDeck("marseille")} />
 				Tarot de Marseille
@@ -46,16 +74,28 @@
 	<div>
 		<h2>{name}</h2>
 		<div class="attribution-container">
-			<a class="attribution" href={attributionLink}>{attributionName}</a>
+			<a class="attribution" rel="noreferrer" href={attributionLink}>{attributionName}</a>
 		</div>
+	</div>
+
+	<div class="selected-card-container">
+		<button class="arrow left" on:click={() => handleArrowClick("previous")} />
+		<img alt={selectedCard.name} src={selectedCard.image} loading="lazy" />
+		<button class="arrow right" on:click={() => handleArrowClick("next")} />
+	</div>
+
+	<div class="card-details-container">
+		<h3>{selectedCard.name}</h3>
+		<p>{selectedCard.interpretation}</p>
+		<p>{selectedCard.reversedInterpretation}</p>
 	</div>
 
 	<div class="cards-container">
 		{#each deck as card (card)}
-			<div class="card">
+			<button class={cardDeckCSS} on:click={() => handleCardClick(card)}>
 				<img alt={card.name} src={card.image} loading="lazy" />
 				<h3>{card.name}</h3>
-			</div>
+			</button>
 		{/each}
 	</div>
 </main>
@@ -80,6 +120,16 @@
 		text-align: center;
 	}
 
+	.attribution:hover {
+		text-decoration: none;
+	}
+
+	.attribution {
+		text-decoration: none;
+		color: inherit;
+		text-decoration: underline;
+	}
+
 	.options-container {
 		display: flex;
 		flex-direction: column;
@@ -102,25 +152,94 @@
 		margin-bottom: 0.3em;
 	}
 
+	.selected-card-container {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		margin: 2em 0;
+	}
+
+	.selected-card-container img {
+		max-width: 290px;
+	}
+
+	.card-details-container {
+		background-color: var(--secondary-bg-color);
+		padding: 1em 1em 2em 1em;
+		margin-left: auto;
+		margin-right: auto;
+		margin-bottom: 4em;
+		border-top: 3px solid var(--main-accent-color);
+	}
+
+	.card-details-container h3,
+	p {
+		margin-left: 10%;
+		margin-right: 10%;
+	}
+
+	.arrow {
+		border: solid var(--main-font-color);
+		border-width: 0 7px 7px 0;
+		padding: 7px;
+		background: none;
+		cursor: pointer;
+		margin: 0 1em;
+	}
+
+	.arrow:hover {
+		border-color: var(--secondary-accent-color);
+	}
+
+	.right {
+		transform: rotate(-45deg);
+		-webkit-transform: rotate(-45deg);
+	}
+
+	.left {
+		transform: rotate(135deg);
+		-webkit-transform: rotate(135deg);
+	}
+
 	.cards-container {
 		display: flex;
 		flex-wrap: wrap;
 		justify-content: center;
-		padding: 1em 3em 2em 3em;
+		padding: 1em;
 		gap: 1em;
+		margin-bottom: 3em;
 	}
 
-	.card {
-		max-width: 200px;
+	.marseille,
+	.rider-waite-smith {
+		max-width: 270px;
+		height: 570px;
 		text-align: center;
+		background: none;
+		border: none;
+		font-family: inherit;
+		font-size: inherit;
+		cursor: pointer;
+		color: inherit;
 	}
 
-	.card img {
+	.rider-waite-smith {
+		max-width: 270px;
+		height: 520px;
+	}
+
+	.marseille img,
+	.rider-waite-smith img {
+		max-width: 90%;
+	}
+
+	.marseille img:hover,
+	.rider-waite-smith img:hover {
 		max-width: 100%;
-		border-radius: 5px;
 	}
 
-	.card h3 {
+	.marseille h3,
+	.rider-waite-smith h3 {
 		margin-top: 0.5em;
 	}
 </style>
